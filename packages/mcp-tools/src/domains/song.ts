3cd65f549e1e03b3a4140508d93f8d032bfc9f61
indusy55 <indusy@outlook.com>
2026-05-25 00:53:05 +0800

    refactor mcp song tool grouping

--- packages/mcp-tools/src/domains/song.ts
+++ packages/mcp-tools/src/domains/song.ts
@@ -13,145 +13,136 @@ import {
   mapSongListSummary,
 } from "../mappers/summary.js";
 
+const songReadMode = z.enum([
+  "detail",
+  "similar",
+  "creators",
+  "music_detail",
+  "dynamic_cover",
+  "copyright_rcmd",
+  "chorus",
+  "red_count",
+  "wiki_summary",
+]);
+
+const songMediaMode = z.enum([
+  "url",
+  "url_v1",
+  "download_url",
+  "download_url_v1",
+  "check",
+  "lyric",
+  "lyric_new",
+]);
+
+const songLibraryMode = z.enum([
+  "personal_fm",
+  "likelist",
+  "recommend_songs",
+  "recommend_resource",
+  "related_playlist",
+]);
+
 export const registerSongTools: ToolRegistrar = (server, { ncm, call }) => {
   server.registerTool(
-    "netease_song_detail",
+    "netease_song_read",
     {
-      description: "song detail",
+      description: "song read",
       annotations: readOnlyAnnotations,
       inputSchema: {
-        ids: z
-          .string()
-          ,
-      },
-    },
-    async ({ ids }) =>
-      call("netease_song_detail", () => ncm.call("song_detail", { ids }), mapSongDetailSummary),
-  );
-
-  server.registerTool(
-    "netease_song_url",
-    {
-      description: "song url",
-      annotations: readOnlyAnnotations,
-      inputSchema: {
-        id: z.union([z.number(), z.string()]),
-        br: z
-          .number()
-          .int()
-          .optional()
-          ,
-      },
-    },
-    async ({ id, br }) => call("netease_song_url", () => ncm.call("song_url", { id, br })),
-  );
-
-  server.registerTool(
-    "netease_song_url_v1",
-    {
-      description: "song url v1",
-      annotations: readOnlyAnnotations,
-      inputSchema: {
-        id: z.union([z.number(), z.string()]),
-        level: z
-          .enum(["standard", "exhigh", "lossless", "hires", "jyeffect", "jymaster", "sky"])
-          .default("standard")
-          ,
-      },
-    },
-    async ({ id, level }) =>
-      call("netease_song_url_v1", () => ncm.call("song_url_v1", { id, level })),
-  );
-
-  server.registerTool(
-    "netease_song_download_url",
-    {
-      description: "song download url",
-      annotations: readOnlyAnnotations,
-      inputSchema: {
-        id: z.union([z.number(), z.string()]),
-        br: z
-          .union([z.number().int(), z.string()])
-          .optional()
-          ,
-      },
-    },
-    async ({ id, br }) =>
-      call("netease_song_download_url", () => ncm.call("song_download_url", { id, br })),
-  );
-
-  server.registerTool(
-    "netease_song_download_url_v1",
-    {
-      description: "song download url v1",
-      annotations: readOnlyAnnotations,
-      inputSchema: {
-        id: z.union([z.number(), z.string()]),
-        level: z
-          .enum(["standard", "exhigh", "lossless", "hires", "jyeffect", "jymaster", "sky"])
-          .default("standard")
-          ,
-      },
-    },
-    async ({ id, level }) =>
-      call("netease_song_download_url_v1", () =>
-        ncm.call("song_download_url_v1", { id, level }),
-      ),
-  );
-
-  server.registerTool(
-    "netease_check_music",
-    {
-      description: "check music",
-      annotations: readOnlyAnnotations,
-      inputSchema: {
-        id: z.union([z.number(), z.string()]),
-        br: z
-          .union([z.number().int(), z.string()])
-          .optional()
-          ,
-      },
-    },
-    async ({ id, br }) => call("netease_check_music", () => ncm.call("check_music", { id, br })),
-  );
-
-  server.registerTool(
-    "netease_lyric",
-    {
-      description: "lyric",
-      annotations: readOnlyAnnotations,
-      inputSchema: {
-        id: z.union([z.number(), z.string()]),
-      },
-    },
-    async ({ id }) => call("netease_lyric", () => ncm.call("lyric", { id })),
-  );
-
-  server.registerTool(
-    "netease_lyric_new",
-    {
-      description: "lyric new",
-      annotations: readOnlyAnnotations,
-      inputSchema: {
-        id: z.union([z.number(), z.string()]),
-      },
-    },
-    async ({ id }) => call("netease_lyric_new", () => ncm.call("lyric_new", { id })),
-  );
-
-  server.registerTool(
-    "netease_simi_song",
-    {
-      description: "simi song",
-      annotations: readOnlyAnnotations,
-      inputSchema: {
-        id: z.union([z.number(), z.string()]),
+        mode: songReadMode.default("detail"),
+        id: z.union([z.number(), z.string()]).optional(),
+        ids: z.string().optional(),
+        songid: z.union([z.number(), z.string()]).optional(),
         limit: z.number().int().min(1).max(100).default(10),
         offset: z.number().int().min(0).default(0),
       },
     },
-    async ({ id, limit, offset }) =>
-      call("netease_simi_song", () => ncm.call("simi_song", { id, limit, offset }), mapSongListSummary),
+    async ({ mode, id, ids, songid, limit, offset }) => {
+      switch (mode) {
+        case "similar":
+          return call("netease_song_read", () => ncm.call("simi_song", { id, limit, offset }), mapSongListSummary);
+        case "creators":
+          return call("netease_song_read", () => ncm.call("song_creators", { id }));
+        case "music_detail":
+          return call("netease_song_read", () => ncm.call("song_music_detail", { id }));
+        case "dynamic_cover":
+          return call("netease_song_read", () => ncm.call("song_dynamic_cover", { id }));
+        case "copyright_rcmd":
+          return call("netease_song_read", () => ncm.call("song_copyright_rcmd", { id, songid }));
+        case "chorus":
+          return call("netease_song_read", () => ncm.call("song_chorus", { id }));
+        case "red_count":
+          return call("netease_song_read", () => ncm.call("song_red_count", { id }));
+        case "wiki_summary":
+          return call("netease_song_read", () => ncm.call("song_wiki_summary", { id }));
+        case "detail":
+        default:
+          return call("netease_song_read", () => ncm.call("song_detail", { ids: ids ?? String(id ?? "") }), mapSongDetailSummary);
+      }
+    },
+  );
+
+  server.registerTool(
+    "netease_song_media",
+    {
+      description: "song media",
+      annotations: readOnlyAnnotations,
+      inputSchema: {
+        mode: songMediaMode.default("url"),
+        id: z.union([z.number(), z.string()]),
+        br: z.union([z.number().int(), z.string()]).optional(),
+        level: z
+          .enum(["standard", "exhigh", "lossless", "hires", "jyeffect", "jymaster", "sky"])
+          .optional(),
+      },
+    },
+    async ({ mode, id, br, level }) => {
+      switch (mode) {
+        case "url_v1":
+          return call("netease_song_media", () => ncm.call("song_url_v1", { id, level: level ?? "standard" }));
+        case "download_url":
+          return call("netease_song_media", () => ncm.call("song_download_url", { id, br }));
+        case "download_url_v1":
+          return call("netease_song_media", () => ncm.call("song_download_url_v1", { id, level: level ?? "standard" }));
+        case "check":
+          return call("netease_song_media", () => ncm.call("check_music", { id, br }));
+        case "lyric":
+          return call("netease_song_media", () => ncm.call("lyric", { id }));
+        case "lyric_new":
+          return call("netease_song_media", () => ncm.call("lyric_new", { id }));
+        case "url":
+        default:
+          return call("netease_song_media", () => ncm.call("song_url", { id, br }));
+      }
+    },
+  );
+
+  server.registerTool(
+    "netease_song_library",
+    {
+      description: "song library",
+      annotations: readOnlyAnnotations,
+      inputSchema: {
+        mode: songLibraryMode.default("recommend_songs"),
+        id: z.union([z.number(), z.string()]).optional(),
+      },
+    },
+    async ({ mode, id }) => {
+      switch (mode) {
+        case "personal_fm":
+          return call("netease_song_library", () => ncm.call("personal_fm"), mapSongListSummary);
+        case "likelist":
+          return call("netease_song_library", () => ncm.call("likelist"), mapLikelistSummary);
+        case "recommend_resource":
+          return call("netease_song_library", () => ncm.call("recommend_resource"), mapRecommendResourceSummary);
+        case "related_playlist":
+          return call("netease_song_library", () => ncm.call("related_playlist", { id }), mapPlaylistListSummary);
+        case "recommend_songs":
+        default:
+          return call("netease_song_library", () => ncm.call("recommend_songs"), mapRecommendSongsSummary);
+      }
+    },
   );
 
   server.registerTool(
@@ -173,10 +164,7 @@ export const registerSongTools: ToolRegistrar = (server, { ncm, call }) => {
       annotations: writeAnnotations,
       inputSchema: {
         id: z.union([z.number(), z.string()]),
-        uid: z
-          .union([z.number(), z.string()])
-          .optional()
-          ,
+        uid: z.union([z.number(), z.string()]).optional(),
         like: z.boolean().default(true),
       },
     },
@@ -184,101 +172,6 @@ export const registerSongTools: ToolRegistrar = (server, { ncm, call }) => {
       call("netease_song_like", () =>
         ncm.call("song_like", { id, uid, like: like ? "true" : "false" }),
       ),
-  );
-
-  server.registerTool(
-    "netease_song_creators",
-    {
-      description: "song creators",
-      annotations: readOnlyAnnotations,
-      inputSchema: {
-        id: z.union([z.number(), z.string()]),
-      },
-    },
-    async ({ id }) => call("netease_song_creators", () => ncm.call("song_creators", { id })),
-  );
-
-  server.registerTool(
-    "netease_song_music_detail",
-    {
-      description: "song music detail",
-      annotations: readOnlyAnnotations,
-      inputSchema: {
-        id: z.union([z.number(), z.string()]),
-      },
-    },
-    async ({ id }) =>
-      call("netease_song_music_detail", () => ncm.call("song_music_detail", { id })),
-  );
-
-  server.registerTool(
-    "netease_song_dynamic_cover",
-    {
-      description: "song dynamic cover",
-      annotations: readOnlyAnnotations,
-      inputSchema: {
-        id: z.union([z.number(), z.string()]),
-      },
-    },
-    async ({ id }) =>
-      call("netease_song_dynamic_cover", () => ncm.call("song_dynamic_cover", { id })),
-  );
-
-  server.registerTool(
-    "netease_song_copyright_rcmd",
-    {
-      description: "song copyright rcmd",
-      annotations: readOnlyAnnotations,
-      inputSchema: {
-        id: z
-          .union([z.number(), z.string()])
-          .optional()
-          ,
-        songid: z
-          .union([z.number(), z.string()])
-          .optional()
-          ,
-      },
-    },
-    async ({ id, songid }) =>
-      call("netease_song_copyright_rcmd", () => ncm.call("song_copyright_rcmd", { id, songid })),
-  );
-
-  server.registerTool(
-    "netease_song_chorus",
-    {
-      description: "song chorus",
-      annotations: readOnlyAnnotations,
-      inputSchema: {
-        id: z.union([z.number(), z.string()]),
-      },
-    },
-    async ({ id }) => call("netease_song_chorus", () => ncm.call("song_chorus", { id })),
-  );
-
-  server.registerTool(
-    "netease_song_red_count",
-    {
-      description: "song red count",
-      annotations: readOnlyAnnotations,
-      inputSchema: {
-        id: z.union([z.number(), z.string()]),
-      },
-    },
-    async ({ id }) => call("netease_song_red_count", () => ncm.call("song_red_count", { id })),
-  );
-
-  server.registerTool(
-    "netease_song_wiki_summary",
-    {
-      description: "song wiki summary",
-      annotations: readOnlyAnnotations,
-      inputSchema: {
-        id: z.union([z.number(), z.string()]),
-      },
-    },
-    async ({ id }) =>
-      call("netease_song_wiki_summary", () => ncm.call("song_wiki_summary", { id })),
   );
 
   server.registerTool(
@@ -316,13 +209,8 @@ export const registerSongTools: ToolRegistrar = (server, { ncm, call }) => {
       annotations: writeAnnotations,
       inputSchema: {
         id: z.union([z.number(), z.string()]),
-        markId: z
-          .union([z.number(), z.string()])
-          .optional()
-          ,
-        data: z
-          .string()
-          ,
+        markId: z.union([z.number(), z.string()]).optional(),
+        data: z.string(),
       },
     },
     async ({ id, markId, data }) =>
@@ -337,142 +225,36 @@ export const registerSongTools: ToolRegistrar = (server, { ncm, call }) => {
       description: "song lyrics mark del [write]",
       annotations: writeAnnotations,
       inputSchema: {
-        id: z
-          .union([z.number(), z.string()])
-          ,
+        id: z.union([z.number(), z.string()]),
       },
     },
     async ({ id }) => call("netease_song_lyrics_mark_del", () => ncm.call("song_lyrics_mark_del", { id })),
   );
 
   server.registerTool(
-    "netease_song_purchased",
+    "netease_song_purchase_history",
     {
-      description: "song purchased [login]",
+      description: "song purchase history [login]",
       annotations: readOnlyAnnotations,
       inputSchema: {
+        mode: z.enum(["purchased", "downlist", "monthdownlist", "singledownlist"]).default("purchased"),
         limit: z.number().int().min(1).max(100).default(10),
         offset: z.number().int().min(0).default(0),
       },
     },
-    async ({ limit, offset }) =>
-      call("netease_song_purchased", () => ncm.call("song_purchased", { limit, offset })),
+    async ({ mode, limit, offset }) => {
+      switch (mode) {
+        case "downlist":
+          return call("netease_song_purchase_history", () => ncm.call("song_downlist", { limit, offset }));
+        case "monthdownlist":
+          return call("netease_song_purchase_history", () => ncm.call("song_monthdownlist", { limit, offset }));
+        case "singledownlist":
+          return call("netease_song_purchase_history", () => ncm.call("song_singledownlist", { limit, offset }));
+        case "purchased":
+        default:
+          return call("netease_song_purchase_history", () => ncm.call("song_purchased", { limit, offset }));
+      }
+    },
   );
 
-  server.registerTool(
-    "netease_song_downlist",
-    {
-      description: "song downlist [login]",
-      annotations: readOnlyAnnotations,
-      inputSchema: {
-        limit: z.number().int().min(1).max(100).default(10),
-        offset: z.number().int().min(0).default(0),
-      },
-    },
-    async ({ limit, offset }) =>
-      call("netease_song_downlist", () => ncm.call("song_downlist", { limit, offset })),
-  );
-
-  server.registerTool(
-    "netease_song_monthdownlist",
-    {
-      description: "song monthdownlist [login]",
-      annotations: readOnlyAnnotations,
-      inputSchema: {
-        limit: z.number().int().min(1).max(100).default(10),
-        offset: z.number().int().min(0).default(0),
-      },
-    },
-    async ({ limit, offset }) =>
-      call("netease_song_monthdownlist", () => ncm.call("song_monthdownlist", { limit, offset })),
-  );
-
-  server.registerTool(
-    "netease_song_singledownlist",
-    {
-      description: "song singledownlist [login]",
-      annotations: readOnlyAnnotations,
-      inputSchema: {
-        limit: z.number().int().min(1).max(100).default(10),
-        offset: z.number().int().min(0).default(0),
-      },
-    },
-    async ({ limit, offset }) =>
-      call("netease_song_singledownlist", () => ncm.call("song_singledownlist", { limit, offset })),
-  );
-
-  server.registerTool(
-    "netease_personal_fm",
-    {
-      description: "personal fm [login]",
-      annotations: readOnlyAnnotations,
-      inputSchema: {},
-    },
-    async () => call("netease_personal_fm", () => ncm.call("personal_fm"), mapSongListSummary),
-  );
-
-  server.registerTool(
-    "netease_like",
-    {
-      description: "like [write]",
-      annotations: writeAnnotations,
-      inputSchema: {
-        id: z.union([z.number(), z.string()]),
-        like: z.boolean().default(true),
-      },
-    },
-    async ({ id, like }) =>
-      call("netease_like", () =>
-        ncm.call("like", { id, like: like ? "true" : "false" }),
-      ),
-  );
-
-  server.registerTool(
-    "netease_likelist",
-    {
-      description: "likelist [login]",
-      annotations: readOnlyAnnotations,
-      inputSchema: {},
-    },
-    async () => call("netease_likelist", () => ncm.call("likelist"), mapLikelistSummary),
-  );
-
-  server.registerTool(
-    "netease_recommend_songs",
-    {
-      description: "recommend songs [login]",
-      annotations: readOnlyAnnotations,
-      inputSchema: {},
-    },
-    async () =>
-      call("netease_recommend_songs", () => ncm.call("recommend_songs"), mapRecommendSongsSummary),
-  );
-
-  server.registerTool(
-    "netease_recommend_resource",
-    {
-      description: "recommend resource [login]",
-      annotations: readOnlyAnnotations,
-      inputSchema: {},
-    },
-    async () =>
-      call(
-        "netease_recommend_resource",
-        () => ncm.call("recommend_resource"),
-        mapRecommendResourceSummary,
-      ),
-  );
-
-  server.registerTool(
-    "netease_related_playlist",
-    {
-      description: "related playlist",
-      annotations: readOnlyAnnotations,
-      inputSchema: {
-        id: z.union([z.number(), z.string()]),
-      },
-    },
-    async ({ id }) =>
-      call("netease_related_playlist", () => ncm.call("related_playlist", { id }), mapPlaylistListSummary),
-  );
 };
